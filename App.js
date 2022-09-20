9e32aff4aa15a62833fcf4f7d6aff75e88cfe922
신 우현 <woo_hyun_1@naver.com>
2022-09-20 23:55:21 +0900

    Create LoginScreen page

--- App.js
+++ App.js
@@ -70,12 +70,11 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Hello Woohyun !">
+          <Section title="Hello Woohyun !!!">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
---
+++ LoginScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View, Text, Image, SafeAreaView, StyleSheet} from 'react-native';
+import type {Node} from 'react';
+
+const LoginScreen: () => Node = () => {
+  return <View></View>;
+};
