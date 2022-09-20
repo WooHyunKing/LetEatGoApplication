4c9db27f5a47add0bd593623e025e365a43dfc8c
신 우현 <woo_hyun_1@naver.com>
2022-09-20 23:58:07 +0900

    Test

--- LoginScreen.js
+++ LoginScreen.js
@@ -3,5 +3,9 @@ import {View, Text, Image, SafeAreaView, StyleSheet} from 'react-native';
 import type {Node} from 'react';
 
 const LoginScreen: () => Node = () => {
-  return <View></View>;
+  return (
+    <View>
+      <View></View>
+    </View>
+  );
 };
